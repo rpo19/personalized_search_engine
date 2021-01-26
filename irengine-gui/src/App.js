af5fd80bc178f72df81bd41f1101e818e1f40105
rpo <pozzir00@gmail.com>
2021-01-26 13:01:14 +0100

    basic search box

--- irengine-gui/src/App.js
+++ irengine-gui/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      queryResults: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) { this.setState({ value: event.target.value }); }
+  handleSubmit(event) {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(
+        { query: { match: { full_text: this.state.value } } })
+    };
+    fetch("http://127.0.0.1:9200/usertweets/_search", requestOptions)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          // this.setState({
+          //   isLoaded: true,
+          //   items: result.items
+          // // });
+          // console.log(result);
+          // console.log(result['hits']['total'].value);
+
+          const hits = result['hits']['hits']
+
+          this.handleResults(hits)
+
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log("fetch error!");
+        }
+      );
+    event.preventDefault();
+  }
+
+  handleResults(hits) {
+    const results = hits.map((hit, idx) => {
+      return (
+        <li key={hit['_source']['id_str']}>
+          <span>
+            {hit['_source']['full_text']}
+          </span>
+        </li>);
+    });
+
+    this.setState({
+      queryResults: results,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Search:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <ol> {this.state.queryResults} </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
